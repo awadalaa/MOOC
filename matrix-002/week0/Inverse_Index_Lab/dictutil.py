a95f886b7f82114cf41af0375c4836f79f2e4bfa
Alaa Awad <alaa.awad.mail@gmail.com>
2015-02-25 18:12:22 -0500

    The Function problems

--- matrix-002/week0/Inverse_Index_Lab/dictutil.py
+++ matrix-002/week0/Inverse_Index_Lab/dictutil.py
@@ -2,3 +2,5 @@
 def dict2list(dct, keylist): return [dct[k] for k in keylist]
 
 def list2dict(L, keylist): return {k: v for (k, v) in zip(keylist, L)}
+
+def listrange2dict(L): return {i:L[i] for i in range(len(L))}
