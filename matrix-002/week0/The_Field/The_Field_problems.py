fae19e809e5e77850eba3c5bfabb1bd068baf7fd
Alaa Awad <alaa.awad.mail@gmail.com>
2015-02-28 13:43:02 -0500

    The Field complete

--- matrix-002/week0/The_Field/The_Field_problems.py
+++ matrix-002/week0/The_Field/The_Field_problems.py
@@ -33,7 +33,7 @@ def my_lists(L):
     >>> my_lists([0,3])
     [[], [1, 2, 3]]
     '''
-    pass
+    return [list(range(1,i+1)) for i in L]
 
 
 
@@ -56,7 +56,7 @@ def myFunctionComposition(f, g):
       >>> myFunctionComposition(a,b) == {'x':'twentyfour','y':'twentyfive'}
       True
     '''
-    pass
+    return {key:g[value] for key,value in f.items()}
 
 
 
@@ -74,7 +74,10 @@ Be sure your procedure works for the empty list.
       >>> mySum([3,5,10])
       18
     '''
-    pass
+    current = 0
+    for x in L:
+        current += x
+    return current
 
 
 
@@ -92,7 +95,10 @@ Be sure your procedure works for the empty list.
       >>> myProduct([-3,2,4])
       -24
     '''
-    pass
+    current = 1
+    for x in L:
+        current *= x
+    return current
 
 
 
@@ -111,7 +117,11 @@ Hint: The value of the Python expression float('infinity') is infinity.
     >>> myMin([0,3,5,-2,-5])
     -5
     '''
-    pass
+    current = -1 * float('inf')
+    for x in L:
+        if (current > x):
+            current=x
+    return current
 
 
 
@@ -129,7 +139,10 @@ Be sure your procedure works for the empty list.
     >>> myConcat(['what','is','up'])
     'whatisup'
     '''
-    pass
+    current = ''
+    for x in L:
+        current+=x
+    return current
 
 
 
@@ -147,17 +160,20 @@ Be sure your procedure works for the empty list.
     >>> myUnion([set(),{3,5},{3,5}])
     {3, 5}
     '''
-    pass
+    current = set({})
+    for _s in L:
+        current|=_s
+    return current
 
 
 
 ## 9: (Problem 9) Complex Addition Practice
 # Each answer should be a Python expression whose value is a complex number.
 
-complex_addition_a = ...
-complex_addition_b = ...
-complex_addition_c = ...
-complex_addition_d = ...
+complex_addition_a = 5 + 3j
+complex_addition_b = 1j
+complex_addition_c = -1+0.001j
+complex_addition_d = 0.001 + 9j
 
 
 
@@ -176,12 +192,12 @@ def transform(a, b, L):
     >>> transform(3,2,[1,2,3])
     [5, 8, 11]
     '''
-    pass
+    return [a*x+b for x in L]
 
 
 
 ## 11: (Problem 11) GF(2) Arithmetic
-GF2_sum_1 = ... # answer with 0 or 1
-GF2_sum_2 = ...
-GF2_sum_3 = ...
+GF2_sum_1 = 1 # answer with 0 or 1
+GF2_sum_2 = 0
+GF2_sum_3 = 0
 
